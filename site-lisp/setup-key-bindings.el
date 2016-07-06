9eab99255624459ea71af0a5d034d824e33484b4
Kei Tsuji <ktsujister@gmail.com>
2016-07-06 12:19:39 +0900

    added C-x 5 to guide-key

--- site-lisp/setup-key-bindings.el
+++ site-lisp/setup-key-bindings.el
@@ -52,7 +52,7 @@
 
 ;; guide-key
 ;; (require 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +" "C-x RET"))
+(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x v" "C-x 8" "C-x +" "C-x RET" "C-x 5"))
 (guide-key-mode 1)
 (setq guide-key/recursive-key-sequence-flag t)
 (setq guide-key/popup-window-position 'bottom)
