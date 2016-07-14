39253437dc803377b6e14b9b76a1b923d95a2d68
Kei Tsuji <ktsujister@gmail.com>
2016-07-14 22:04:58 +0900

    removing modeline-git-branch

--- site-lisp/my-defaults.el
+++ site-lisp/my-defaults.el
@@ -50,13 +50,13 @@
 
 (setq auto-save-default nil)
 
-(require 'modeline-git-branch)
-(modeline-git-branch-mode 1)
+;; (require 'modeline-git-branch)
+;; (modeline-git-branch-mode 1)
 
 ;; fix for slow emacs when saving files
-(setq vc-handled-backends ())
+;; (setq vc-handled-backends ())
 
-(remove-hook 'find-file-hook 'vc-find-file-hook)
-(remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
+;; (remove-hook 'find-file-hook 'vc-find-file-hook)
+;; (remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
 
 (provide 'my-defaults)
