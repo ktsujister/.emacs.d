d7b85c1c65ee77a2bf347a325f81543963ada890
Kei Tsuji <ktsujister@gmail.com>
2018-11-27 19:00:41 +0900

    updated clojure mode config

--- site-lisp/setup-clojure.el
+++ site-lisp/setup-clojure.el
@@ -52,6 +52,14 @@
 
 (require 'clj-refactor)
 
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
 (setq cljr-suppress-middleware-warnings t)
 (setq cljr-favor-prefix-notation nil)
 
