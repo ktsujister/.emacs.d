5b56a1b67efb82fbd8d407de5db46c206652141b
Kei Tsuji <ktsujister@gmail.com>
2019-08-30 14:46:53 +0900

    change setting for eww

--- site-lisp/setup-eww.el
+++ site-lisp/setup-eww.el
@@ -5,18 +5,38 @@
     (funcall orig start end fg)))
 (advice-add 'shr-colorize-region :around 'shr-colorize-region--disable)
 (advice-add 'eww-colorize-region :around 'shr-colorize-region--disable)
-(defun eww-disable-color ()
-  "eww で文字色を反映させない"
-  (interactive)
-  (setq-local eww-disable-colorize t)
-  (eww-reload))
-(defun eww-enable-color ()
-  "eww で文字色を反映させる"
-  (interactive)
-  (setq-local eww-disable-colorize nil)
-  (eww-reload))
-(setq eww-search-prefix "https://www.google.co.jp/search?q=")
+;; (defun eww-disable-color ()
+;;   "eww で文字色を反映させない"
+;;   (interactive)
+;;   (setq-local eww-disable-colorize t)
+;;   (eww-reload))
 
+;; (defun eww-enable-color ()
+;;   "eww で文字色を反映させる"
+;;   (interactive)
+;;   (setq-local eww-disable-colorize nil)
+;;   (eww-reload))
+
+;; ;; https://futurismo.biz/archives/2950/
+;; (defun eww-disable-images ()
+;;   "eww で画像表示させない"
+;;   (interactive)
+;;   (setq-local shr-put-image-function 'shr-put-image-alt)
+;;   (eww-reload))
+;; (defun eww-enable-images ()
+;;   "eww で画像表示させる"
+;;   (interactive)
+;;   (setq-local shr-put-image-function 'shr-put-image)
+;;   (eww-reload))
+;; (defun shr-put-image-alt (spec alt &optional flags)
+;;   (insert alt))
+;; ;; はじめから非表示
+;; (defun eww-mode-hook--disable-image ()
+;;   (setq-local shr-put-image-function 'shr-put-image-alt))
+;; (add-hook 'eww-mode-hook 'eww-mode-hook--disable-image)
+
+(setq eww-search-prefix "https://www.google.co.jp/search?q=")
+(setq shr-inhibit-images t)
 (setq browse-url-browser-function 'eww-browse-url)
 
 (provide 'setup-eww)
