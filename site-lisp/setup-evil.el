dac65daaf158531f8f33ab9d75c4187b9c9d84a7
Kei Tsuji <ktsujister@gmail.com>
2019-12-02 13:30:48 +0900

    add fine undo

--- site-lisp/setup-evil.el
+++ site-lisp/setup-evil.el
@@ -2,5 +2,6 @@
 (setq evil-cross-lines t
       evil-search-module 'evil-search
       evil-ex-search-vim-style-regexp t
-      evil-want-C-u-scroll t)
+      evil-want-C-u-scroll t
+      evil-want-fine-undo t)
 (provide 'setup-evil)
