45428495a21f419c1a3b2b241868eb2158b2071f
Kei Tsuji <ktsujister@gmail.com>
2020-01-22 11:13:27 +0900

    adding yml and toml

--- init.el
+++ init.el
@@ -53,7 +53,9 @@
     ("\\.cs$\\|\\.cpp$\\|\\.c$\\|\\.h$" . c++-mode)
     ("\\.xsd$\\|\\.xml$\\|\\.xsx$" . nxml-mode)
     ("\\.jsp$" . html-mode)
+    ("\\.yml$" . yaml-mode)
     ("\\.yaml$" . yaml-mode)
+    ("\\.toml$" . conf-toml-mode)
     ("\\.txt$" . fundamental-mode))
   auto-mode-alist))
 
