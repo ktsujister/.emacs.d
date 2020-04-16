23f2ad3515275ceaab76149dff51f9aee6993743
Kei Tsuji <ktsujister@gmail.com>
2020-04-16 13:20:59 +0900

    commented out unused function and modify default state from emacs to normal

--- site-lisp/setup-evil.el
+++ site-lisp/setup-evil.el
@@ -1,34 +1,41 @@
-(defun turn-on-evil-mode-fixed-escape ()
-  "Turn on Evil in the current buffer AND `evil-esc-mode'. This makes C-[ work
-like <escape> when using `evil-local-mode'."
-  (interactive)
-  (turn-on-evil-mode)
-  (evil-esc-mode 1))
+;; (defun turn-on-evil-mode-fixed-escape ()
+;;   "Turn on Evil in the current buffer AND `evil-esc-mode'. This makes C-[ work
+;; like <escape> when using `evil-local-mode'."
+;;   (interactive)
+;;   (turn-on-evil-mode)
+;;   (evil-esc-mode 1))
 
-(defun toggle-evil-local-mode ()
-"Toggle on and off evil mode in local buffer."
-(interactive)
-(if evil-local-mode
-    (turn-off-evil-mode)
-  (turn-on-evil-mode-fixed-escape)))
+;; (defun toggle-evil-local-mode ()
+;; "Toggle on and off evil mode in local buffer."
+;; (interactive)
+;; (if evil-local-mode
+;;     (turn-off-evil-mode)
+;;   (turn-on-evil-mode-fixed-escape)))
 
-(setq evil-default-state 'emacs)
+;; (setq evil-default-state 'emacs)
 
-;; normal state
-(dolist (mode '(clojure-mode
-		rust-mode
-		python-mode
-		sh-mode
-		js-mode
-		emacs-lisp-mode
-		java-mode
-		conf-toml-mode
-		yaml-mode
-		markdown-mode
-		c-mode
-		c++-mode
-		make-mode))
-  (add-to-list 'evil-normal-state-modes mode))
+;; ;; normal state
+;; (dolist (mode '(clojure-mode
+;; 		rust-mode
+;; 		python-mode
+;; 		sh-mode
+;; 		js-mode
+;; 		emacs-lisp-mode
+;; 		java-mode
+;; 		conf-toml-mode
+;; 		yaml-mode
+;; 		markdown-mode
+;; 		c-mode
+;; 		c++-mode
+;; 		make-mode))
+;;   (add-to-list 'evil-normal-state-modes mode))
+
+;; emacs state
+(dolist (mode '(org-mode
+		magit-mode
+		mew-summary-mode
+		))
+  (add-to-list 'evil-emacs-state-modes mode))
 
 ;; insert state
 (add-to-list 'evil-insert-state-modes 'cider-repl-mode)
