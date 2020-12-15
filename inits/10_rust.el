29af9fa0887fd097ddc6e294d8a11e8068f34b38
Kei Tsuji <ktsujister@gmail.com>
2020-12-15 16:59:26 +0900

    use rustic mode and simplify

--- inits/10_rust.el
+++ inits/10_rust.el
@@ -1,5 +1,7 @@
 (use-package rustic
-  :mode (("\\.rs\\'" . rustic-mode))
+  :ensure t
+  :defer t
+  :mode ("\\.rs$" . rustic-mode)
   :commands (rustic-mode)
   :config
   (use-package quickrun
@@ -12,38 +14,3 @@
     :defer t
     :ensure t)
   )
-;; (use-package rust-mode
-;;   :mode (("\\.rs\\'" . rust-mode))
-;;   :commands (rust-mode)
-;;   :init
-;;   (add-to-list 'exec-path (expand-file-name "~/.cargo/bin/"))
-;;   )
-
-;; ;; (use-package cargo
-;; ;;   :after rust-mode
-;; ;;   :config
-;; ;;   (add-hook 'rust-mode-hook 'cargo-minor-mode))
-
-;; (use-package racer
-;;   :after rust-mode
-;;   :if (executable-find "racer")
-;;   ;; :diminish racer-mode
-;;   :init
-;;   (add-hook 'rust-mode-hook 'racer-mode)
-;;   ;; (setq racer-rust-src-path
-;;   ;; 	(concat (string-trim
-;;   ;; 		 (shell-command-to-string "rustc --print sysroot"))
-;;   ;; 		"/lib/rustlib/src/rust/src"))
-;;   :config
-;;   (when (featurep 'company)
-;;     (add-hook 'racer-mode-hook 'company-mode)
-;;     (set (make-local-variable 'eldoc-idle-delay) 5)
-;;     (setq company-tooltip-align-annotations t))
-;;   (add-hook 'racer-mode-hook 'eldoc-mode))
-
-;; ;; (use-package flycheck-rust
-;; ;;   :if (featurep 'flycheck)
-;; ;;   :after (rust-mode flycheck)
-;; ;;   :init
-;; ;;   (add-hook 'rust-mode-hook 'flycheck-mode)
-;; ;;   (add-hook 'flycheck-mode-hook 'flycheck-rust-setup))
