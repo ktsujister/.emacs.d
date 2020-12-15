edc284371b062447852dcc5a63d9667291e240b9
Kei Tsuji <ktsujister@gmail.com>
2020-12-15 16:46:32 +0900

    moved some hooks settings to package settings

--- site-lisp/setup-hooks.el
+++ site-lisp/setup-hooks.el
@@ -44,8 +44,6 @@
  (lambda ()
    (paredit-mode 1)
    (highlight-parentheses-mode)))
-(require 'yasnippet)
-(yas-reload-all)
 (add-hook
  'sh-mode-hook
  (lambda ()
@@ -62,18 +60,6 @@
    ;; remove keybinding for sh-if to use company-mode
    (define-key sh-mode-map "\C-c\C-i" nil)
    ))
-
-(add-hook
- 'magit-mode-hook
- (lambda ()
-   ;; remove keybinding for magit-add-change-log-entry for anything
-   (define-key magit-status-mode-map "\C-xa" nil)))
-
-(add-hook
- 'magit-hode-hook
- (lambda ()
-   ;; remove keybinding for magit-add-change-log-entry for anything
-   (define-key magit-log-mode-map "\C-xa" nil)))
 
 (add-hook
  'emacs-startup-hook
