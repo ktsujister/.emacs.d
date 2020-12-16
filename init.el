25a6e647aa166fbf8633898fde3800fb529821bc
Kei Tsuji <ktsujister@gmail.com>
2020-12-16 11:34:23 +0900

    added yaml mode

--- init.el
+++ init.el
@@ -41,8 +41,6 @@
     ("\\.cs$\\|\\.cpp$\\|\\.c$\\|\\.h$" . c++-mode)
     ("\\.xsd$\\|\\.xml$\\|\\.xsx$" . nxml-mode)
     ("\\.jsp$" . html-mode)
-    ("\\.yml$" . yaml-mode)
-    ("\\.yaml$" . yaml-mode)
     ("\\.toml$" . conf-toml-mode)
     ("\\.txt$" . fundamental-mode))
   auto-mode-alist))
---
+++ inits/40_yaml.el
@@ -0,0 +1,13 @@
+(use-package yaml-mode)
+;; (setq
+;;  auto-mode-alist
+;;  (append
+;;   '(("\\.cs$" . csharp-mode)
+;;     ("\\.cs$\\|\\.cpp$\\|\\.c$\\|\\.h$" . c++-mode)
+;;     ("\\.xsd$\\|\\.xml$\\|\\.xsx$" . nxml-mode)
+;;     ("\\.jsp$" . html-mode)
+;;     ("\\.yml$" . yaml-mode)
+;;     ("\\.yaml$" . yaml-mode)
+;;     ("\\.toml$" . conf-toml-mode)
+;;     ("\\.txt$" . fundamental-mode))
+;;   auto-mode-alist))
