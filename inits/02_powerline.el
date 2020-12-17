9aa2097c818ffe882fab0adc707ca50d44fa9581
Kei Tsuji <ktsujister@gmail.com>
2020-12-17 13:22:19 +0900

    modified

--- inits/02_powerline.el
+++ inits/02_powerline.el
@@ -1,9 +1,11 @@
-(use-package powerline)
+(use-package powerline
+  :ensure t)
 (use-package powerline-evil
+  :after powerline
   :config
   (powerline-evil-center-color-theme)
-;; (powerline-default-theme)
-;; (powerline-center-evil-theme)
-;; (powerline-evil-vim-color-theme)
-;; (powerline-vim-theme)
+  ;; (powerline-evil-vim-color-theme)
+  ;; (powerline-default-theme)
+  ;; (powerline-center-evil-theme)
+  ;; (powerline-vim-theme)
   )
--- inits/40_other.el
+++ inits/40_other.el
@@ -1,3 +1,5 @@
 (use-package yaml-mode)
 
+(use-package web-mode)
+
 (use-package terraform-mode)
