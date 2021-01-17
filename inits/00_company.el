5efde5d34bdeb3323290f932df5ce6bdb8fc21a5
Kei Tsuji <ktsuji@sigfoss.com>
2021-01-18 00:44:18 +0900

    add yasnippet to company-backends

--- inits/00_company.el
+++ inits/00_company.el
@@ -1,14 +1,19 @@
 (use-package company
   :init
-  (setq company-selection-wrap-around t)
+  (global-company-mode)
   :bind
   (:map company-active-map
 	("M-n" . nil)
 	("M-p" . nil)
 	("C-n" . company-select-next)
 	("C-p" . company-select-previous)
-	("C-h" . nil))
+	("C-h" . nil)
+	("C-c y /" . company-yasnippet)
+	)
   :config
-      (global-company-mode))
+  (setq company-selection-wrap-around t
+	completion-ignore-case t)
+  (add-to-list 'company-backends 'company-yasnippet)
+  )
 
 (global-set-key "\C-c\C-i" 'company-complete)
