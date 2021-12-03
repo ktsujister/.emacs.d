6db6b49fab137e6db5e4e24048ca98347f7a1770
Kei Tsuji <ktsuji@sigfoss.com>
2021-12-03 14:10:45 +0900

    updated tree-sitter config

--- inits/09_treesitter.el
+++ inits/09_treesitter.el
@@ -1,18 +1,8 @@
 (use-package tree-sitter-langs
-  :defer t
   :ensure t)
+
 (use-package tree-sitter
-  :defer t
-  :ensure t
-  :after (tree-sitter-langs)
+  :after tree-sitter-langs
   :config
   (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
-  (tree-sitter-require 'rust)
-  (tree-sitter-require 'python)
-  ;; (add-hook 'rustic-mode-hook #'tree-sitter-hl-mode)
-  ;; (add-hook 'python-mode-hook #'tree-sitter-hl-mode)
-  ;; (add-to-list 'tree-sitter-major-mode-language-alist '(python-mode . python))
-  ;; (add-to-list 'tree-sitter-major-mode-language-alist '(elpy-mode . python))
-  ;; (tree-sitter-require 'ruby)
-  )
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
