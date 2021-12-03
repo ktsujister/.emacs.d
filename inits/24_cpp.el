aec63add34c5e72f12a55d95dd8582797b36e66e
Kei Tsuji <ktsuji@sigfoss.com>
2021-12-03 14:08:58 +0900

    add google-c-style

--- inits/24_cpp.el
+++ inits/24_cpp.el
@@ -2,3 +2,24 @@
 
 (use-package cmake-mode
   :mode ("CMakeLists\\.txt$" . cmake-mode))
+
+(defun cc-mode-init ()
+  (google-set-c-style)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 4)
+  (setq indent-level 4)
+  (setq tab-width 4)
+  (c-set-offset 'case-label 0))
+
+(use-package google-c-style
+  :config
+  (add-hook 'c-mode-hook 'cc-mode-init)
+  (add-hook 'c++-mode-hook 'cc-mode-init)
+  (add-hook 'c-mode-common-hook 'google-make-newline-indent)
+  ;; (add-hook 'c-mode-hook 'google-set-c-style)
+  ;; (add-hook 'c++-mode-hook 'google-set-c-style)
+  ;; (setq indent-tabs-mode t)
+  ;; (setq c-basic-offset 4)
+  ;; (c-set-offset 'case-label 0)
+  )
+
