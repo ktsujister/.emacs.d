a8c5071d1e169cfb87cd03da7c6c438cb6799306
Kei Tsuji <ktsuji@sigfoss.com>
2022-05-06 19:55:59 +0900

    set color-theme-solarized in inits

--- inits/92_themes.el
+++ inits/92_themes.el
@@ -1,3 +1,11 @@
-(use-package solarized-theme
+;; (use-package solarized-theme
+;;   :config
+;;   (load-theme 'solarized-dark t))
+(use-package color-theme-solarized
+  :straight (color-theme-solarized :type git :host github :repo "sellout/emacs-color-theme-solarized")
+  ;; :defer t
+  :ensure t
   :config
-  (load-theme 'solarized-dark t))
+  ;; (load-theme 'solarized t)
+  (setq solarized-termcolors 256)
+  )
