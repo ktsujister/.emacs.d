369fec812b192ce324333d09271980ee32444a86
Kei Tsuji <ktsuji@sigfoss.com>
2023-04-10 15:19:03 +0900

    add defer

--- inits/92_themes.el
+++ inits/92_themes.el
@@ -1,6 +1,8 @@
 (use-package color-theme-solarized
   :straight (color-theme-solarized :type git :host github :repo "sellout/emacs-color-theme-solarized")
   :ensure t
+  :defer t
+  ;; :load-path "straight/build/color-theme-solarized"
   :init
   (setq solarized-termcolors 256)
   (load-theme 'solarized t))
