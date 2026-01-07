51f961ced3aec01f6cc1ef9aa63b2ae0a2405cc7
Kei Tsuji <ktsuji@sigfoss.com>
2026-01-07 23:41:37 +0900

    comment out frame-background-mode config.

--- init.el
+++ init.el
@@ -50,7 +50,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(frame-background-mode 'dark)
+ ;; '(custom-safe-themes
+ ;;   '("55c81b8ddb2b6c3fa502b1ff79fa8fed6affe362447d5e72388c7d160a2879d0" default))
+ ;; '(frame-background-mode 'dark)
  '(highlight-parentheses-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(magit-pull-arguments nil)
  '(org-agenda-files '("~/memo/todo.org"))
